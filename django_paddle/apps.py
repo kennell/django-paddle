046c2227b45b4fa904195c105706e25894d3fa03
kennell <kevin@kennell.de>
2020-04-11 19:20:31 +0200

    rm ready method in app config

--- django_paddle/apps.py
+++ django_paddle/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class DjangoPaddleConfig(AppConfig):
     name = 'django_paddle'
     verbose_name = 'Django Paddle'
-
-    def ready(self):
-        from django_paddle.signals import *
