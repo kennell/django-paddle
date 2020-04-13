a1dcf6240905d2ec606fe7c4c7caf5f7ad917888
kennell <kevin@kennell.de>
2020-04-14 00:24:05 +0200

    rm managers

--- django_paddle/managers.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class PaddlePaymentManager(models.Manager):
-
-    def create_paddle_(self, title):
-        book = self.create(title=title)
-        # do something with the book
-        return book
