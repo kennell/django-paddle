140407952a392196a44f369c4d61abdb41f5e5f3
kennell <kevin@kennell.de>
2020-04-30 01:12:23 +0200

    fix command class name

--- django_paddle/management/commands/paddle_sync_plans.py
+++ django_paddle/management/commands/paddle_sync_plans.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django_paddle.models import PaddlePlan
 
 
-class PaddleSyncPlans(BaseCommand):
+class Command(BaseCommand):
     help = 'Sync plans'
 
     def handle(self, *args, **options):
