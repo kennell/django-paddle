22c2ba310de0cc60f69442b253eab5b09b21666a
kennell <kevin@kennell.de>
2021-11-28 23:38:11 +0100

    unittests

--- tests/models/test_paddle_subscription.py
+++ tests/models/test_paddle_subscription.py
@@ -1,12 +1,14 @@
 import responses
+from datetime import timedelta
 from django.test import TestCase
 from django_paddle.models import PaddleSubscription, PaddlePlan
+from django.utils import timezone
 
 
 class TestPaddleSubscription(TestCase):
 
     def setUp(self):
-        self.plan = PaddleSubscription.objects.create(
+        self.subscription = PaddleSubscription.objects.create(
             id=12345,
             plan=PaddlePlan.objects.create(
                 id=123,
@@ -21,7 +23,8 @@ class TestPaddleSubscription(TestCase):
             update_url='https://checkout.paddle.com/subscription/update?foo=bar&qux=baz',
             cancel_url='https://checkout.paddle.com/subscription/cancel?foo=bar&qux=baz',
             state='active',
-            signup_date='2020-01-01 20:20:20'
+            signup_date='2020-01-01 20:20:20',
+            cancellation_effective_date=None
         )
 
     @responses.activate
@@ -33,9 +36,9 @@ class TestPaddleSubscription(TestCase):
                 'success': True
             }
         )
-        self.plan.cancel()
-        self.plan.refresh_from_db()
-        self.assertEqual(self.plan.state, 'deleted')
+        self.subscription.cancel()
+        self.subscription.refresh_from_db()
+        self.assertEqual(self.subscription.state, 'deleted')
 
     @responses.activate
     def test_subscription_pause(self):
@@ -46,3 +49,90 @@ class TestPaddleSubscription(TestCase):
     def test_subscription_unpause(self):
         # TBI
         self.assertTrue(True)
+
+
+class TestPaddleSubscriptionIsCanceled(TestCase):
+
+    """
+    Tests the is_canceled property
+    """
+
+    def setUp(self):
+        self.subscription = PaddleSubscription.objects.create(
+            id=12345,
+            plan=PaddlePlan.objects.create(
+                id=123,
+                name='Plan Foo',
+                billing_type='month',
+                billing_period=1,
+                trial_days=14
+            ),
+            user_id=12345,
+            user_email='foo@example.com',
+            marketing_consent=True,
+            update_url='https://checkout.paddle.com/subscription/update?foo=bar&qux=baz',
+            cancel_url='https://checkout.paddle.com/subscription/cancel?foo=bar&qux=baz',
+            state='active',
+            signup_date='2020-01-01 20:20:20',
+            cancellation_effective_date=None
+        )
+
+    def test_is_canceled_cancellation_effective_date_is_null(self):
+        self.subscription.cancellation_effective_date = None
+        self.subscription.save()
+        self.assertFalse(self.subscription.is_canceled)
+
+    def test_is_canceled_cancellation_effective_date_is_set(self):
+        self.subscription.cancellation_effective_date = timezone.now()
+        self.subscription.save()
+        self.assertTrue(self.subscription.is_canceled)
+
+
+class TestPaddleSubscriptionIsActive(TestCase):
+
+    """
+    Tests the is_active property
+    """
+
+    def setUp(self):
+        self.subscription = PaddleSubscription.objects.create(
+            id=12345,
+            plan=PaddlePlan.objects.create(
+                id=123,
+                name='Plan Foo',
+                billing_type='month',
+                billing_period=1,
+                trial_days=14
+            ),
+            user_id=12345,
+            user_email='foo@example.com',
+            marketing_consent=True,
+            update_url='https://checkout.paddle.com/subscription/update?foo=bar&qux=baz',
+            cancel_url='https://checkout.paddle.com/subscription/cancel?foo=bar&qux=baz',
+            state='active',
+            signup_date='2020-01-01 20:20:20',
+            cancellation_effective_date=None
+        )
+
+    def test_is_active(self):
+        self.subscription.state = 'active'
+        self.subscription.save()
+        self.assertTrue(self.subscription.is_active)
+
+    def test_is_active_deleted_no_cancellation_date(self):
+        self.subscription.state = 'deleted'
+        self.subscription.cancellation_effective_date = None
+        self.subscription.save()
+        self.assertFalse(self.subscription.is_active)
+
+    def test_is_active_cancellation_date_in_future(self):
+        self.subscription.state = 'deleted'
+        self.subscription.cancellation_effective_date = timezone.now() + timedelta(days=3)
+        self.subscription.save()
+        self.assertTrue(self.subscription.is_active)
+
+    def test_is_active_cancellation_date_in_past(self):
+        self.subscription.state = 'deleted'
+        self.subscription.cancellation_effective_date = timezone.now() - timedelta(days=3)
+        self.subscription.save()
+        self.assertFalse(self.subscription.is_active)
